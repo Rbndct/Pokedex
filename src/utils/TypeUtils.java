1f6dcecbbeb86e03d757f392d0b6cd8458f3fcfe
Rbee <rbenedict.maagma@gmail.com>
2025-06-10 12:15:26 +0800

    Refactor TypeUtils class for improved readability and prevent instantiation

--- src/utils/TypeUtils.java
+++ src/utils/TypeUtils.java
@@ -2,31 +2,33 @@ package utils;
 
 public class TypeUtils {
 
-  private static final String[] VALID_TYPES = {
-      "normal", "fire", "water", "electric", "grass", "ice",
-      "fighting", "poison", "ground", "flying", "psychic", "bug",
-      "rock", "ghost", "dragon", "dark", "steel", "fairy"
-  };
+    private static final String[] VALID_TYPES = {
+        "normal", "fire", "water", "electric", "grass", "ice",
+        "fighting", "poison", "ground", "flying", "psychic", "bug",
+        "rock", "ghost", "dragon", "dark", "steel", "fairy"
+    };
 
-  public static boolean isValidType(String type) {
-    boolean found = false;
-    for (String validType : VALID_TYPES) {
-      if (validType.equals(type)) {
-        found = true;
-        break;
-      }
+    // Prevent instantiation
+    private TypeUtils() {
+        throw new UnsupportedOperationException("Utility class");
     }
-    return found;
-  }
 
-
-  public static String capitalize(String s) {
-    String result;
-    if (s == null || s.isEmpty()) {
-      result = s;
-    } else {
-      result = s.substring(0, 1).toUpperCase() + s.substring(1);
+    public static boolean isValidType(String type) {
+        boolean found = false;
+        for (String validType : VALID_TYPES) {
+            if (validType.equals(type)) {
+                found = true;
+                break;
+            }
+        }
+        return found;
     }
-    return result;
-  }
+
+    public static String capitalize(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        } else {
+            return s.substring(0, 1).toUpperCase() + s.substring(1);
+        }
+    }
 }
