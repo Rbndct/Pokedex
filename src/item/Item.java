9036de85e4ce3a1d9135bb7a72aa7668f0130038
Rbndct <rbenedict.maagma@gmail.com>
2025-06-26 18:29:36 +0800

    Refactor Item class to improve documentation and add display method for better item representation

--- src/item/Item.java
+++ src/item/Item.java
@@ -1,5 +1,12 @@
 package item;
 
+import utils.DisplayHelper;
+import java.util.List;
+
+/**
+ * The type Item.
+ */
+@SuppressWarnings("squid:S106")
 public class Item {
 
     private final int buyingPrice;
@@ -10,8 +17,19 @@ public class Item {
     private final int sellingPrice;
     private int stock;
 
+    /**
+     * Instantiates a new Item.
+     *
+     * @param name         the name
+     * @param category     the category
+     * @param description  the description
+     * @param effect       the effect
+     * @param buyingPrice  the buying price
+     * @param sellingPrice the selling price
+     * @param stock        the stock
+     */
     public Item(String name, String category, String description, String effect,
-        int buyingPrice, int sellingPrice, int stock) {
+                int buyingPrice, int sellingPrice, int stock) {
         this.name = name;
         this.category = category;
         this.description = description;
@@ -21,38 +39,91 @@ public class Item {
         this.stock = stock;
     }
 
-    // Getters
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets category.
+     *
+     * @return the category
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Gets effect.
+     *
+     * @return the effect
+     */
     public String getEffect() {
         return effect;
     }
 
+    /**
+     * Gets buying price.
+     *
+     * @return the buying price
+     */
     public int getBuyingPrice() {
         return buyingPrice;
     }
 
+    /**
+     * Gets selling price.
+     *
+     * @return the selling price
+     */
     public int getSellingPrice() {
         return sellingPrice;
     }
 
+    /**
+     * Gets stock.
+     *
+     * @return the stock
+     */
     public int getStock() {
         return stock;
     }
 
-    // Setters
+    /**
+     * Sets stock.
+     *
+     * @param stock the stock
+     */
     public void setStock(int stock) {
         this.stock = stock;
     }
 
+    /**
+     * Display.
+     */
+    public void display() {
+        String formatRow = "%-25s %-20s %-35s\n";
+        List<String> lines = DisplayHelper.wrapText(effect, 35);
+
+        // Print first line with all columns
+        System.out.printf(formatRow, name, category, lines.getFirst());
+
+        // Print remaining wrapped lines of the effect
+        for (int i = 1; i < lines.size(); i++) {
+            System.out.printf(formatRow, "", "", lines.get(i));
+        }
+    }
 }
